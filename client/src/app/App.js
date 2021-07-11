85252f18065b681407ec38c4386a3562f2bf7053
Michael Pappas <mixalis.pappas23@gmail.com>
2021-07-11 11:37:01 +1000

    addon: add the nav bar on the app.js

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from '../view/home';
+import Nav from '../components/nav';
+
 
 function App() {
 	return (
 			<Router>
 				<div className='App'>
+					<Nav />
 					<Switch>
-              <Route path='/' exact component={Home} />
+             			 <Route path='/' exact component={Home} />
 				
 					</Switch>
 				</div>
